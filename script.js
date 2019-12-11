a0ede450f2c99e379fc99de70c6ae873e0e5906e
cendiin <45700386+cendiin@users.noreply.github.com>
2019-12-11 08:12:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function setup(){
-    window.operatorUsed = true
+    window.operatorUsed = false
+    window.decimalUsed = false
     }
 
 function sqrt(){
-    value = document.getElementById("text-field").value
-    document.getElementById("text-field").value = Math.sqrt(value)
-    
+    document.getElementById("text-field").value = Math.sqrt(document.getElementById("text-field").value)
 }
 
 function equals() {
@@ -17,6 +16,7 @@ function equals() {
     {'-': (a, b) => a - b}
     ]
     const value = document.getElementById("text-field").value
+    let lastString = value[value.length - 1]
     //reg exp splits string into array of numbers and operator strings to output to string parser
     // | seperates individual operators, \ escapes to use the operator as a string, the final '+' looks for continous characters
     let split = value.split(/(\/|\*|\+|\-|\^)+/)
@@ -34,35 +34,46 @@ function equals() {
             }
     })
 
-    console.log(calculation)
-
-    operations.forEach(function(i){
-        calculation.forEach(function(j){
-             if (Object.keys(i) == j) {
-                //map function to variable
-                currentOperation = Object.values(i)[0]
-                if (calculationStack.length < 1){
-                    calculationStack[0] = currentOperation(calculation[calculation.indexOf(j)-1], calculation[calculation.indexOf(j)+1])
-                }
-                else if (calculationStack.length >= 1){
-                    calculationStack[0] = currentOperation(calculationStack[0], calculation[calculation.indexOf(j)+1])
-                }
-                else {
-                    calculationStack[0] = currentOperation(calculation[calculation.indexOf(j)-1], calculationStack[0])
-                }
-              }
-        })
-    })
-    console.log(calculation)
-    if (calculation == NaN){
-        document.getElementById("text-field").value = 0
+    if (calculation.length <= 1 || lastString == '^' ||lastString == '*' || lastString == '/' || lastString == '+' || lastString == '-'){
+        document.getElementById("text-field").value = value
     }
     else {
-        document.getElementById("text-field").value = calculationStack[0]
+        operations.forEach(function(i){
+            calculation.forEach(function(j){
+                 if (Object.keys(i) == j) {
+                    //map function to variable
+                    currentOperation = Object.values(i)[0]
+                    if (calculationStack.length < 1){
+                        calculationStack[0] = currentOperation(calculation[calculation.indexOf(j)-1], calculation[calculation.indexOf(j)+1])
+                    }
+                    else if (calculationStack.length >= 1){
+                        calculationStack[0] = currentOperation(calculationStack[0], calculation[calculation.indexOf(j)+1])
+                    }
+                    else {
+                        calculationStack[0] = currentOperation(calculation[calculation.indexOf(j)-1], calculationStack[0])
+                    }
+                  }
+            })
+        })
+        if (value.includes('.')){
+            document.getElementById("text-field").value = calculationStack[0]
+            decimalUsed = true   
+        } else {
+            document.getElementById("text-field").value = calculationStack[0] 
+        }   
     }
 }
 
 function deleteOne() {
+    let value = document.getElementById("text-field").value
+    let lastString = value[value.length - 1]
+    if (lastString == '^' || lastString == '-' || lastString == '+' || lastString == '*' || lastString == '/'){
+        operatorUsed = false
+        decimalUsed = true
+    }
+    if (lastString == '.'){
+        decimalUsed = false
+    }
     if (document.getElementById("text-field").value.length == 1 || document.getElementById("text-field").value == '0'){
         document.getElementById("text-field").value = '0';
     }
@@ -72,6 +83,8 @@ function deleteOne() {
 }
 
 function clearScreen() {
+    operatorUsed = false
+    decimalUsed = false
     document.getElementById("text-field").value = '0';
 }
 
@@ -80,31 +93,29 @@ function buttons(str){
     let value = document.getElementById("text-field").value
     let lastString = value[value.length - 1]
 
-    //used to flag if an operator was used to limit multiple decimals on the same number
+    // //used to flag if an operator was used to limit multiple decimals on the same number
     if (str == '*' || str == '/' || str == '+' || str == '-' || str == '^'){
         operatorUsed = true
+        decimalUsed = false
     }
-    //doesnt allow multiple operators in a row
     if ((lastString == ('^') || lastString == ('-') || lastString == ('+') || lastString == ('*') || lastString == ('/')) && (operatorStr)) {
-    }
-    //doesnt allow multiple decimals in a row
-    else if (lastString == '.' && str == '.'){
-    }
-    //doesnt allow multiple decimals in the same number
-    else if (str == '.' && operatorUsed == false){
-    }
-    //if an operator is used lets a decimal be added to the string and sets the flag false to block another decimal
-    else if (str == '.' && operatorUsed == true) {
-        document.getElementById("text-field").value += str;
-        operatorUsed = false
-    }
-    //replaces 0 with integers
-    else if (document.getElementById("text-field").value == '0'){
-        document.getElementById("text-field").value = '';
-        document.getElementById("text-field").value += str;
-    }
-    else {
-        document.getElementById("text-field").value += str;
-    }
+        //doesnt allow multiple operators in a row when last string matches
+    }   else if (lastString == '.' && str == '.'){
+            //doesnt allow multiple decimals in a row when last string matches
+        }
+        else if ((str == '.' && decimalUsed == false) || (str == '.' && decimalUsed == false && operatorUsed == true )){
+            //doesnt allow multiple decimals in the same number
+            document.getElementById("text-field").value += str;
+            decimalUsed = true
+        }
+        else if (document.getElementById("text-field").value == '0'){
+            //replaces default 0 with integers
+            document.getElementById("text-field").value = '';
+            document.getElementById("text-field").value += str;
+        }
+        else if (str !== '.') {
+             //allows numbers only
+            document.getElementById("text-field").value += str;
+        }
 }
 
