e9d363e375e0aeeaeca7d610af18e6a46d71fe18
cendiin <cendiin@gmail.com>
2023-12-14 14:40:06 -0500

    added support gateway

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('button').addEventListener('click', ()=> {
+    correct = true
+    while (correct) {
+    let code = prompt("Please provide the code given by the person on the phone.");
+    if (code == "help") {
+        window.location.href = "support.html";
+        correct = false;
+    } else {
+        alert("Please put in the correct code.");
+    }
+}});
+
+
+
