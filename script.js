5509f5abb31b062c1a2f29ea500070718ed25fe9
cendiin <cendiin@gmail.com>
2023-12-14 14:55:29 -0500

    fixing support button

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.getElementById('button').addEventListener('click', ()=> {
-    correct = true
-    while (correct) {
-    let code = prompt("Please provide the code given by the person on the phone.");
-    if (code == "help") {
-        window.location.href = "support.html";
-        correct = false;
+document.getElementById("button").addEventListener("click", () => {
+  correct = true;
+  while (correct) {
+    let code = prompt(
+      "Please provide the code given by the person on the phone."
+    );
+    if (code == null) {
+        break
+    } else if (code == "help") {
+      window.location.href = "support.html";
+      correct = false;
     } else {
-        alert("Please put in the correct code.");
+      alert("Please put in the correct code.");
     }
-}});
-
-
-
+  }
+});
